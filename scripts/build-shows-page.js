9d0b6ec5fb9f74795e1447c4ef508c69bac0ae9e
i-am-lp <lp.phillips95@gmail.com>
2024-08-23 12:05:34 -0400

    shows javascript

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -6,3 +6,63 @@ const shows = [
     { date: 'Fri Nov 29 2024', venue: "Moscow Center", location:"San Francisco, CA"},
     { date: 'Wed Dec 18 2024', venue: "Press Club", location:"San Francisco, CA"}
 ];
+
+function createShowsPage(shows) {
+    const showEl = document.createElement('div');
+    showEl.classList.add('shows__total');
+
+    const heading = document.createElement('h2');
+    heading.classList.add('shows');
+    heading.textContent = 'Shows';
+
+    const titleDate = document.createElement('p');
+    titleDate.classList.add('shows__total--title--date');
+    titleDate.textContent = 'DATE';
+    showEl.appendChild(titleDate);
+
+    const dateEl = document.createElement('p');
+    dateEl.classList.add('shows__total--date');
+    dateEl.textContent = shows.date;
+    showEl.appendChild(dateEl);
+
+    const titleVenue = document.createElement('p');
+    titleVenue.classList.add('shows__total--title--venue');
+    titleVenue.textContent = 'VENUE';
+    showEl.appendChild(titleVenue);
+
+    const venueEl = document.createElement('p');
+    venueEl.classList.add('shows__total--venue');
+    venueEl.textContent = shows.venue;
+    showEl.appendChild(venueEl);
+
+    const titleLocation = document.createElement('p');
+    titleLocation.classList.add('shows__total--title--location');
+    titleLocation.textContent = 'LOCATION';
+    showEl.appendChild(titleLocation);
+
+    const locationEl = document.createElement('p');
+    locationEl.classList.add('shows__total--lecation');
+    locationEl.textContent = shows.location;
+    showEl.appendChild(locationEl);
+
+    const button = document.createElement('button');
+    button.classList.add('shows__total--button');
+    button.textContent = 'BUY TICKETS';
+    showEl.appendChild(button);
+
+    return showEl;
+}
+
+function renderShows() {
+    const buildShowsEl = document.querySelector('#shows-js');
+
+    buildShowsEl.innerHTML="";
+
+    for (let i=0; i < shows.length; i++) {
+        const card = createShowsPage(shows[i]);
+        buildShowsEl.appendChild(card);
+    }
+}
+
+const showCardEl = document.querySelector('#shows-js');
+renderShows();
